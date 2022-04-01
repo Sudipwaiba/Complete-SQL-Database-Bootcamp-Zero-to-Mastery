d8be39a99725a1ba426db020e061a8133d655fd9
Sudipwaiba <97788054+Sudipwaiba@users.noreply.github.com>
2022-04-01 12:16:43 +0530

    Update questions.sql

--- SQL Deep Dive/Null Values/questions.sql
+++ SQL Deep Dive/Null Values/questions.sql
@@ -4,9 +4,12 @@
 * Question: 
 * Assuming a students minimum age for the class is 15, what is the average age of a student?
 */
+select AVG(age) from student
+where MIN(AGE)=15;
 
 /*
 * DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
 * Question: 
 * Replace all empty first or last names with a default?
 */
+select coalesce(first_name, last_name, empty) from students
