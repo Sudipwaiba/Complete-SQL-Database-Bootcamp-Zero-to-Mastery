0f066c0577412299c10a469bc346f14b0ef6befc
Sudipwaiba <97788054+Sudipwaiba@users.noreply.github.com>
2022-04-01 11:54:01 +0530

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -7,6 +7,8 @@
 * Include people that are 50
 * that are from either Japan or Australia
 */
+select first_name, age from Customers
+where age<30 OR age>=50 AND (country='Japan' OR country='Australia');
 
 /*
 * DB: Store
