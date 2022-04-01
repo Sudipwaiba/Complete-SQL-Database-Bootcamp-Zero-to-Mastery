5a4306705d712db14d7a73477c55837495c2205c
Sudipwaiba <97788054+Sudipwaiba@users.noreply.github.com>
2022-04-01 12:42:07 +0530

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -1,11 +1,8 @@
 -- Who between the ages of 30 and 50 has an income less than 50 000?
 -- (include 30 and 50 in the results)
-
-/*
-* Write your query here
-*/
+select name from salary
+where age between (30,50) and salary<50000;
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
-/*
-* Write your query here
-*/
+select AVG(salary) from salary 
+where age between (20,50);
