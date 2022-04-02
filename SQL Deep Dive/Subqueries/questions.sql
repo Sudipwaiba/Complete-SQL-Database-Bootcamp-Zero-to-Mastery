5dbd0b227fe8639ed3a107655ab62e1b83f64a4f
Sudipwaiba <97788054+Sudipwaiba@users.noreply.github.com>
2022-04-02 17:06:59 +0530

    Update questions.sql

--- SQL Deep Dive/Subqueries/questions.sql
+++ SQL Deep Dive/Subqueries/questions.sql
@@ -5,10 +5,25 @@
 * Question: Get all orders from customers who live in Ohio (OH), New York (NY) or Oregon (OR) state
 * ordered by orderid
 */
-
+select c.firstname, c.last_name, o.orderid
+from orders as o, (select customerid, firstname, lastname, state from customers ) as c
+where o.customerid= c.customerid and c.state in ('OH','NY','OR')
+ORDER by o.orderid;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Filter employees who have emp_no 110183 as a manager
 */
+select emp_no, first_name, last_name
+from employees
+where emp_no in (
+  select emp_no 
+  from dept_emp
+  where dept_no in (
+    select dept_no
+    from dept_manager
+    where emp_no =110183)
+  )
+  order by emp_no;
+  
